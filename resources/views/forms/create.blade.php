e773c5117873bc32882f41ac9c39f342e275d32d
Thumin <96730005+Thu-Min@users.noreply.github.com>
2025-01-29 13:07:08 +0700

    fix: header, create form input button sticky

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -5,7 +5,7 @@
     <form action="{{ route('forms.store') }}" method="POST" class="space-y-6">
         @csrf
 
-        <div class="flex justify-between items-center">
+        <div class="bg-gray-200 p-5 rounded-lg flex justify-between items-center sticky z-50 top-0">
             <h1 class="text-2xl font-bold">Create New Form</h1>
 
             <div class="flex space-x-2">
@@ -14,7 +14,7 @@
             </div>
         </div>
 
-        <div class="bg-white shadow-md border border-gray-400 rounded-lg p-6 space-y-4">
+        <div class="bg-white shadow-md border border-gray-400 rounded-lg p-6 space-y-4 relative">
             <div>
                 <label class="block text-sm font-medium text-gray-700">Form Name</label>
                 <input type="text" name="name" required class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
@@ -25,7 +25,7 @@
             </div>
         </div>
 
-        <div id="inputs-container" class="space-y-6">
+        <div id="inputs-container" class="space-y-6 relative">
         </div>
     </form>
 </div>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,14 +1,13 @@
 <nav class="flex items-center justify-between flex-wrap bg-blue-400 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <span class="font-semibold text-xl tracking-tight">Custom Forms</span>
+        <a href="{{ route('dashboard') }}" class="font-semibold text-xl tracking-tight">
+            Custom Forms
+        </a>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
             <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
                 Docs
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-                Examples
             </a>
         </div>
         <div>
